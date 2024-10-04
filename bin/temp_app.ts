05a0f4d150d003565a956f6f2e14a58136c4ff6f
HBobertz <bobertzx@gmail.com>
2024-10-04 17:16:49 -0400

    super nested app

--- bin/temp_app.ts
+++ bin/temp_app.ts
@@ -2,9 +2,10 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { TempAppStack } from '../lib/temp_app-stack';
+import { AttributeType } from 'aws-cdk-lib/aws-dynamodb';
 
 const app = new cdk.App();
-new TempAppStack(app, 'TempAppStack', {
+const rootStack = new TempAppStack(app, 'TempAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
@@ -19,3 +20,39 @@ new TempAppStack(app, 'TempAppStack', {
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+// // NestedStack A
+// let nestA = new cdk.NestedStack(rootStack, 'NestedStackA');
+// new cdk.aws_s3.Bucket(nestA, "WebsiteBucketA", {});
+// new cdk.aws_s3.Bucket(nestA, "WebsiteBucketB", {});
+// new cdk.aws_dynamodb.Table(nestA, "TableA", {
+//   partitionKey: { name: 'id', type: cdk.aws_dynamodb.AttributeType.STRING }
+// });
+
+// // nestB
+// let nestB = new cdk.NestedStack(rootStack, 'NestedStackB');
+// new cdk.aws_sns.Topic(nestB, "NotificationTopic");
+// new cdk.aws_sqs.Queue(nestB, "MessageQueue");
+
+// // nestC in nestA
+// let nestC = new cdk.NestedStack(nestA, 'NestedStackC');
+// new cdk.aws_s3.Bucket(nestC, "nestCBucket");
+
+// // nestD in nestB
+// let nestD = new cdk.NestedStack(nestB, 'NestedStackD');
+// new cdk.aws_dynamodb.Table(nestD, 'nestDTable', {
+//   partitionKey: {name: 'part-name', type: AttributeType.STRING}
+// });
+// // nestE in nestC
+// let nestE = new cdk.NestedStack(nestC, 'NestedStackE');
+// new cdk.aws_cloudwatch.Alarm(nestE, "CPUUtilizationAlarm", {
+//   metric: new cdk.aws_cloudwatch.Metric({
+//     namespace: 'AWS/EC2',
+//     metricName: 'CPUUtilization',
+//   }),
+//   threshold: 70,
+//   evaluationPeriods: 2,
+// });
+// // nestF in nestE
+// let nestF = new cdk.NestedStack(nestE, 'NestedStackF');
+// new cdk.aws_s3.Bucket(nestF, 'nestFBucket', {})
--- lib/temp_app-stack.ts
+++ lib/temp_app-stack.ts
@@ -6,11 +6,6 @@ export class TempAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TempAppQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new cdk.aws_s3.Bucket(this, 'rootBucket', {});
   }
 }
